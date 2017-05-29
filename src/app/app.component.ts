597ca61063be61bd064f1285252bae14ee641c35
Adam Bodie <aduo3000@yahoo.com>
2017-05-29 12:56:38 -0700

    Added about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Untitled East Coast Project';
+  places = ['Washington D.C.', ' Baltimore', ' New York '];
+  photo = 'https://placehold.it/600x450/?text=East Coast Project';
 }
