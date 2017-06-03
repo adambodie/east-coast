e6b3869faf614648a167a9a07f0f8d6caaa6b228
Adam Bodie <aduo3000@yahoo.com>
2017-06-02 17:44:23 -0700

    created loop for carousel photos

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 
+export class Picture {
+  id: number;
+  image: string;
+  alt: string;
+  title: string;
+  description: string;
+}
+
+const PICTURES: Picture[] = [
+  {id: 1, image: 'https://placehold.it/600x450/?text=First Slide', alt: 'First Slide', title: "Title One", description: 'Random description One'},
+  {id: 2, image: 'https://placehold.it/600x450/?text=Second Slide', alt: 'Second Slide', title: "Title Two", description: 'Random description Two'},
+  {id: 3, image: 'https://placehold.it/600x450/?text=Third Slide', alt: 'Third Slide', title: "Title Three", description: 'Random description Three'},
+]
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
-  name = "Adam Bodie @";
-  year = 2017;
-
+  pictures = PICTURES;
 }
