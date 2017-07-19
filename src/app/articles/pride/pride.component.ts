17d9ea334d897c0033b5fa17a27cc779b44ba064
adambodie <aduo3000@yahoo.com>
2017-07-18 18:46:41 -0700

    Updated  art, philly and pride components

--- src/app/articles/pride/pride.component.ts
+++ src/app/articles/pride/pride.component.ts
@@ -10,7 +10,8 @@ import { PictureService } from '../../shared/picture.service';
 })
 export class PrideComponent implements OnInit {
 	pridePictures: Picture[];
-	link = "https://s3-us-west-2.amazonaws.com/nadm2.bodiewebdesign.com/";        
+	link = "https://s3-us-west-2.amazonaws.com/nadm2.bodiewebdesign.com/";
+	date = "June 18, 2017";        
 	constructor(private pictureService: PictureService) { }
 
 	getPictures(): void {
