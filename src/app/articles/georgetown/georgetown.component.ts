d83b0737aeb445238eddbba6853f203f4e9bd936
adambodie <aduo3000@yahoo.com>
2017-07-20 16:26:51 -0700

    Updated capitol/georgetown components

--- src/app/articles/georgetown/georgetown.component.ts
+++ src/app/articles/georgetown/georgetown.component.ts
@@ -9,11 +9,21 @@ import { PictureService } from '../../shared/picture.service';
   providers: [PictureService]
 })
 export class GeorgetownComponent implements OnInit {
-
-	constructor(private pictureService: PictureService) { }
-	link = "https://s3-us-west-2.amazonaws.com/nadm2.bodiewebdesign.com/"; 
+	  georgetownPictures: Picture[];
+	  iThaiPictures: Picture[];
+	  potomacPictures: Picture[];
+	  link = "https://s3-us-west-2.amazonaws.com/nadm2.bodiewebdesign.com/";
+	  date = "June 19, 2017";
+	  constructor(private pictureService: PictureService) { }	       
+	
+	  getPictures(): void {
+		this.pictureService.getGeorgetownPictures().then(pictures => this.georgetownPictures = pictures);
+		this.pictureService.getIThaiPictures().then(pictures => this.iThaiPictures = pictures);
+		this.pictureService.getPotomacPictures().then(pictures => this.potomacPictures = pictures);					
+	}
+	
 	ngOnInit() {
-  
+		this.getPictures();
 	}
 
 }
