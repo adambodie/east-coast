eeac266e11906b8ba73cba2a51e0ddd96387011d
Adam Bodie <aduo3000@yahoo.com>
2017-07-30 11:56:01 -0700

    Added empire feature pictures

--- src/app/articles/empire/empire.component.ts
+++ src/app/articles/empire/empire.component.ts
@@ -5,7 +5,21 @@ import { PictureService } from '../../shared/picture.service';
 const IMAGES = [
   {'name': '432 Park Avenue', 'image': 'empire-feature2', 'description': 'xyz'},
   {'name': 'Central Park', 'image': 'empire-feature3', 'description': 'xyz'},
-  {'name': 'Bank of America Building', 'image': 'empire-feature4', 'description': 'xyz'},
+	{'name': 'Bank of America Building', 'image': 'empire-feature4', 'description': 'xyz'},
+  {'name': 'One57', 'image': 'empire-feature8', 'description': 'xyz'},	
+	{'name': '30 Rockefeller Plaza', 'image': 'empire-feature17', 'description': 'xyz'},
+	{'name': '383 Madison Avenue', 'image': 'empire-feature29', 'description': 'xyz'},
+	{'name': 'Chrysler Building', 'image': 'empire-feature6', 'description': 'xyz'},
+	{'name': 'MetLife Building', 'image': 'empire-feature22', 'description': 'xyz'},
+	{'name': 'Trump World Tower', 'image': 'empire-feature16', 'description': 'xyz'},
+	{'name': '801 Lexington Avenue', 'image': 'empire-feature13', 'description': 'xyz'},
+	{'name': 'One World Trade Center', 'image': 'empire-feature1', 'description': 'xyz'},
+	{'name': 'Three World Trade Center', 'image': 'empire-feature5', 'description': 'xyz'},
+	{'name': '30 Park Place', 'image': 'empire-feature15', 'description': 'xyz'},
+	{'name': '8 Spruce Street', 'image': 'empire-feature11', 'description': 'xyz'},
+	{'name': 'Met Life Tower', 'image': 'empire-feature51', 'description': 'xyz'},
+	{'name': 'Statue of Liberty', 'image': 'empire-feature-liberty', 'description': 'xyz'},
+  {'name': 'Ellis Island', 'image': 'empire-feature-ellis', 'description': 'xyz'},									
  
 ];
 
@@ -29,7 +43,6 @@ export class EmpireComponent implements OnInit {
 	public refreshValue(value:any):void {
 		this.value = value;
 		this.show = true;
-		console.log(this.show);
 	}
 
 	getPictures(): void {
