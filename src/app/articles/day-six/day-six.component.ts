8705a8dfc0b4021748e2c3d73afd14b9a0503499
adambodie <aduo3000@yahoo.com>
2017-08-09 19:23:26 -0700

    Updated liberty/times components

--- src/app/articles/day-six/day-six.component.ts
+++ src/app/articles/day-six/day-six.component.ts
@@ -19,20 +19,20 @@ export class DaySixComponent implements OnInit {
 		{
 			title: "9/11 Memorial", 
 			link: "/six/wtc", 
-			image: "wtc5.jpg", 
-			paragraph: ""
+			image: "wtc11.jpg", 
+			paragraph: "One of the worst days in American History, definitely one of the worst in my lifetime, was the horrific attacks on 9/11.  Being in New York City, right where the attacks occurred, makes it all the more emotional.  Click the link to see how New York honored the lives lost on 9/11."
 		},
 		{
 			title: "Statue of Liberty", 
 			link: "/six/liberty", 
-			image: "liberty50.jpg", 
-			paragraph: ""
+			image: "liberty60.jpg", 
+			paragraph: "Built in 1886, the Statue of Liberty is one of New York's premier destinations, a symbol of liberty and a welcoming sign for all, from the immigrants who make their way to America, to those who love our country.  Click below to see the adventures at the Statue of Liberty."
 		},
 		{
 			title: "Times Square", 
 			link: "/six/times", 
-			image: "times30.jpg", 
-			paragraph: ""
+			image: "times76.jpg", 
+			paragraph: "No place in America provides more excitement in a little place more than Times Square, the crossroads of the world to many who visit just to have a good time.  See the zany adventures at Times Square by clicking the link below."
 		}
 	]
 	constructor(private pictureService: PictureService) { }
