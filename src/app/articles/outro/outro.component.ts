333c4fbd5e4ebdafb2fab7d2b925876dcd3de6dd
adambodie <aduo3000@yahoo.com>
2017-08-15 17:13:08 -0700

    Updated outro component

--- src/app/articles/outro/outro.component.ts
+++ src/app/articles/outro/outro.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Picture } from '../../shared/picture';
+import { PictureService } from '../../shared/picture.service';
 
 @Component({
   selector: 'app-outro',
   templateUrl: './outro.component.html',
-  styleUrls: ['./outro.component.css']
+  styleUrls: ['../article.component.css'],
+  providers: [PictureService]
 })
 export class OutroComponent implements OnInit {
 
-  constructor() { }
+	outroPictures: Picture[];
+	link = "https://s3-us-west-2.amazonaws.com/nadm2.bodiewebdesign.com/";
+	date = "June 23, 2017";        
+	constructor(private pictureService: PictureService) { }
 
-  ngOnInit() {
-  }
+	getPictures(): void {
+		this.pictureService.getOutroPictures().then(pictures => this.outroPictures = pictures);
+	}
+
+	ngOnInit(): void {
+		this.getPictures();
+	}
 
 }
