6328cdd65897311d7774bbc021b19b9eb093f970
Adam Bodie <aduo3000@yahoo.com>
2017-08-30 18:40:04 -0700

    Updated components, styles

--- src/app/articles/intro/intro.component.ts
+++ src/app/articles/intro/intro.component.ts
@@ -15,15 +15,15 @@ export class IntroComponent implements OnInit {
   link = "https://s3-us-west-2.amazonaws.com/nadm2.bodiewebdesign.com/";  
   date = "June 17, 2017";
   famousAls = [
-    {name:'Al Green', image: 'green.jpg'},
-    {name: 'Al Roker', image: 'roker.jpg'},
-    {name: 'Al Pacino', image: 'pacino.jpg'},
-    {name: 'Al Gore', image: 'gore.jpg'},
-    {name: 'Big Gay Al', image: 'big-gay-al.jpg'},
-    {name: 'Weird Al Yankovic', image: 'yankovic.jpg'},
-    {name: 'Al Capone', image: 'capone.jpg'},
-    {name: 'Al Franken', image: 'franken.jpg'},
-    {name: 'Al Davis', image: 'davis.jpg'}
+    {name:'Al Green', image: 'green.jpg', description: 'Soul singer, songwriter and producer'},
+    {name: 'Al Roker', image: 'roker.jpg', description: 'TV personality of NBC\'s Today Show'},
+    {name: 'Al Pacino', image: 'pacino.jpg', description: 'Academy Award winning Actor'},
+    {name: 'Al Gore', image: 'gore.jpg', description: 'Vice President of the United States under Bill Clinton and Environmental Activist'},
+    {name: 'Big Gay Al', image: 'big-gay-al.jpg', description: 'Flamboyantly gay character from South Park'},
+    {name: 'Weird Al Yankovic', image: 'yankovic.jpg', description: 'Singer, songwriter, parodist of many popular songs'},
+    {name: 'Al Capone', image: 'capone.jpg', description: 'Notorious Gangster during the Prohibition Era'},
+    {name: 'Al Franken', image: 'franken.jpg', description: 'TV writer, comedian and current U.S. Senator from Minnesota'},
+    {name: 'Al Davis', image: 'davis.jpg', description: 'Former NFL coach and owner of the Oakland Raiders'}
     ];
 
 
--- src/app/articles/jersey/jersey.component.ts
+++ src/app/articles/jersey/jersey.component.ts
@@ -15,15 +15,15 @@ export class JerseyComponent implements OnInit {
 	link = "https://s3-us-west-2.amazonaws.com/nadm2.bodiewebdesign.com/";        
 	constructor(private pictureService: PictureService) { }
   famousJerseys = [
-    {name:'Buzz Aldrin', image: 'aldrin.jpg'},
-    {name: 'Jon Bon Jovi', image: 'bon-jovi.jpg'},
-    {name: 'Whitney Houston', image: 'houston.jpg'},
-    {name: 'Jerry Lewis', image: 'jerry-lewis.jpg'},
-    {name: 'Jack Nicholson', image: 'nicholson.jpg'},
-    {name: 'Shaquille O\'Neal', image: 'o-neal.jpg'},
-    {name: 'Frank Sinatra', image: 'sinatra.jpg'},
-    {name: 'Bruce Springsteen', image: 'springsteen.jpg'},
-    {name: 'John Travolta', image: 'travolta.jpg'}
+    {name:'Buzz Aldrin', image: 'aldrin.jpg', description: 'Astronaut, one of the first humans to walk on the Moon'},
+    {name: 'Jon Bon Jovi', image: 'bon-jovi.jpg', description: 'Musician, frontman for the rock group Bon Jovi'},
+    {name: 'Whitney Houston', image: 'houston.jpg', description: 'R&B Singer and Actress'},
+    {name: 'Jerry Lewis', image: 'jerry-lewis.jpg', description: 'Actor and Comedian, host of the Muscular Dystrophy Association Telethons'},
+    {name: 'Jack Nicholson', image: 'nicholson.jpg', description: 'Academy Award winning Actor, most nominated Actor in Academy Award history.'},
+    {name: 'Shaquille O\'Neal', image: 'o-neal.jpg', description: 'Hall of Fame Basketball Player for the Los Angeles Lakers and other teams'},
+    {name: 'Frank Sinatra', image: 'sinatra.jpg', description: 'Singer, Actor and Producer, one of the most influential artists in the 20th Century'},
+    {name: 'Bruce Springsteen', image: 'springsteen.jpg', description: 'Singer-songwriter of the E Street Band known as "The Boss"'},
+    {name: 'John Travolta', image: 'travolta.jpg', description: 'Academy Award nominated Actor, dancer, singer'}
     ];
 	getPictures(): void {
 		this.pictureService.getJerseyPictures().then(pictures => this.jerseyPictures = pictures);
